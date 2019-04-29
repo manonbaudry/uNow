667d5276ca820bdb938809dfc18a493aaa1a3bf0
Manon Baudry <man.baudry@gmail.com>
2019-04-29 15:43:08 +0200

    Email Validator with bugs

---
+++ src/main/java/com/uNow/annotations/UniqueEmailValidator.java
@@ -0,0 +1,30 @@
+package com.uNow.annotations;
+
+import com.uNow.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowire;
+import org.springframework.beans.factory.annotation.Configurable;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+
+@Configurable(autowire = Autowire.BY_TYPE, dependencyCheck = true)
+public class UniqueEmailValidator implements ConstraintValidator<UniqueEmail, String> {
+
+
+    private UserRepository userRepository;
+
+    public UniqueEmailValidator(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public void initialize(UniqueEmail constraintAnnotation) {
+
+    }
+
+    @Override
+    public boolean isValid(String email, ConstraintValidatorContext context) {
+        return email != null && userRepository.findByEmail(email) == null;
+    }
+}
