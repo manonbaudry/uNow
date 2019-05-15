176de826a7f44213af7f98e0585c74ab8baceec2
Manon Baudry <man.baudry@gmail.com>
2019-05-15 15:47:24 +0100

    add crossOrigin ActivityController

--- src/main/java/com/uNow/controller/ActivityController.java
+++ src/main/java/com/uNow/controller/ActivityController.java
@@ -11,8 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin
-@RestController
+@RestController()
 @RequestMapping("/activity")
 public class ActivityController {
 
