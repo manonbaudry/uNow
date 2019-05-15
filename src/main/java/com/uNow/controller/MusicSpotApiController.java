ae06b4ef2f6622c9d19f820104b18e69f40724f6
Manon Baudry <man.baudry@gmail.com>
2019-05-15 11:56:17 +0100

    step 2 spotify api

--- src/main/java/com/uNow/controller/MusicSpotApiController.java
+++ src/main/java/com/uNow/controller/MusicSpotApiController.java
@@ -2,6 +2,8 @@ package com.uNow.controller;
 
 
 import com.uNow.repositories.MusicSpotApiRepository;
+import org.apache.commons.codec.EncoderException;
+import org.apache.commons.codec.binary.Base64;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+
 
 
 @RestController
@@ -26,9 +29,11 @@ public class MusicSpotApiController {
 
     @RequestMapping
     @PostMapping
-    public void spotifyAuth(@RequestParam("code") String code, @RequestParam("state") String state) throws IOException {
+    public void spotifyAuth(@RequestParam("code") String code, @RequestParam("state") String state) throws IOException, EncoderException {
         if (state != null) {
-            HttpPost post = new HttpPost("https://accounts.spotify.com/api/token?client_id=d3dafa68bb434601b56cb999de50ce59&client_secret=57af408d7b3f43cc9fc3728a9101cb89&grant_type=authorization_code&code=" + code + "&redirect_uri=https%3A%2F%2Funow-api.herokuapp.com/%2Fcallback/%2FrefreshToken");
+            HttpPost post = new HttpPost("https://accounts.spotify.com/api/token?grant_type=authorization_code&code=" + code + "&redirect_uri=https%3A%2F%2Funow-api.herokuapp.com/%2Fcallback/%2FrefreshToken");
+            post.setHeader("Content-Type", "application/x-www-form-urlencoded");
+            post.setHeader("Authorization", "Basic" + new Base64().encode("d3dafa68bb434601b56cb999de50ce59") + ":" + new Base64().encode("57af408d7b3f43cc9fc3728a9101cb89"));
             CloseableHttpClient closeableHttpClient = HttpClients.createDefault();
             closeableHttpClient.execute(post);
         }
