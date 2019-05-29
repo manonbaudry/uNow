427430d83c013e794e2ea2df900bf7e3ce5b9948
Manon Baudry <man.baudry@gmail.com>
2019-05-29 16:22:56 +0100

    fix bug

--- src/main/java/com/uNow/repositories/MusicSpotApiRepository.java
+++
@@ -1,8 +0,0 @@
-package com.uNow.repositories;
-
-import com.uNow.entities.MusicSpotApi;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MusicSpotApiRepository extends JpaRepository<MusicSpotApi, Long> {
-
-}
