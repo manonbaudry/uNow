72f5649e5770262cda96d97ec2888450062c148b
Manon Baudry <man.baudry@gmail.com>
2019-05-29 16:05:50 +0100

    delete methods Api spotify

--- src/main/java/com/uNow/controller/UserController.java
+++ src/main/java/com/uNow/controller/UserController.java
@@ -4,15 +4,10 @@ import com.uNow.entities.User;
 import com.uNow.exceptions.AlreadyExistEmailException;
 import com.uNow.exceptions.UserNotFoundException;
 import com.uNow.repositories.UserRepository;
-import org.apache.commons.codec.binary.Base64;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
 import java.util.List;
 
 @CrossOrigin()
@@ -61,22 +56,6 @@ public class UserController {
         return userRepository.findById(userUpdated.getId());
     }
 
-    @CrossOrigin
-    @PostMapping("/callBack/{id}")
-    public void spotifyAuth(@PathVariable("id") long id, @RequestParam("code") String code, @RequestParam("state") String state) throws IOException {
-        if (state != null) {
-            HttpPost post = new HttpPost("https://accounts.spotify.com/api/token?grant_type=authorization_code&code=" + code + "&redirect_uri=https%3A%2F%2Funow-api.herokuapp.com/%2FrefreshToken/%2F" + id);
-            post.setHeader("Content-Type", "application/x-www-form-urlencoded");
-            post.setHeader("Authorization", "Basic" + new Base64().encode("d3dafa68bb434601b56cb999de50ce59".getBytes()) + ":" + new Base64().encode("57af408d7b3f43cc9fc3728a9101cb89".getBytes()));
-            CloseableHttpClient closeableHttpClient = HttpClients.createDefault();
-            closeableHttpClient.execute(post);
-        }
-    }
 
-    @CrossOrigin
-    @GetMapping("/refreshToken/{id}")
-    public void getAccessToken(@PathVariable("id") long id, @RequestParam("access_token") String accessToken, @RequestParam("token_type") String tokenType, @RequestParam("scope") String scope, @RequestParam("expires_in") int expiresIn, @RequestParam("refresh_token") String refreshToken) {
-        userRepository.findById(id).setRefreshToken(refreshToken);
-    }
 
 }
